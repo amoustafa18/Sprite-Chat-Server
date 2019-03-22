ce607fcac2af75ce44d7a514be87e5a1d130afa5
aem278 <aem278@cornell.edu>
2019-03-22 14:19:02 -0400

    Update .gitingore. Project setup

--- src/main/java/io/github/amoustafa18/Server.java
+++ src/main/java/io/github/amoustafa18/Server.java
@@ -1,4 +1,13 @@
 package io.github.amoustafa18;
 
 public class Server {
+
+	public static void main(String[] args) {
+		Server server = new Server();
+		server.run();
+	}
+
+	private void run() {
+		
+	}
 }
